a5d9895e03fe33b29d5389ed4a748495164a4439
1DARWISH8 <126087325+1DARWISH8@users.noreply.github.com>
2024-02-28 21:15:24 +0530

    Create verifytoken.js

---
+++ WEB_DEV/BACKEND/INTERN/server/middlewares/verifytoken.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+
+function verifyToken(req,res,next)
+{
+    // token verification logic
+        // get bearer token from headers of req object
+        const bearerToken = req.headers.authorization;
+        // get token
+        if(bearerToken)
+        {
+            const Token = bearerToken.split(' ')[1]
+            // verify token
+            let decodedToken = jwt.verify(Token,process.env.SECRET_KEY)
+            console.log(decodedToken)
+            console.log(Token)
+            next()
+        }
+        else
+        {
+            res.send({message:"User unauthorised"})
+        }
+}
+
+module.exports=verifyToken
